badf746bd4e9ae12dd2de443b43582292f289c21
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-07-13 10:42:38 +0700

    Update navigator

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: './',
+        extensions: ['.ios.js', '.android.js', '.ts', ',tsx', '.js', '.json'],
+        alias: {
+          '^@/(.+)': ([, name]) => `./src/${name}`,
+          '@assets': './assets',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/modules/home/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeModule = () => {
+  return (
+    <View>
+      <Text>HomeModule</Text>
+    </View>
+  )
+}
+
+export default HomeModule
+
+const styles = StyleSheet.create({})
